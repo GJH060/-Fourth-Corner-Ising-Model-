2c2824836268f0ee12d84634e0c92c6c889c128e
Fhui28 <fhui28@gmail.com>
2026-06-18 15:01:19 +1000

    modifying toy example after meeting with Jiahao on 18 June 2026

--- toyexample.R
+++ toyexample.R
@@ -10,13 +10,13 @@ source("Code/FCIR/estimate_FCIR.r")
 ##------------------------
 #' # Simulate multivariate binary data from FCIR model
 ##------------------------
-Ns <- 800
+Ns <- 400
 Ps <- 60
 
 # Global fixed parameters
 L = 3          
 K = 2          
-seed = 42      
+seed = 2026      
 
 
 # generate_fully_dense_fcir_data(N = Ns, 
@@ -58,6 +58,15 @@ fit_FCIR_lassolambda <- estimate_penalized_FCIR(Y = Y[,,1],
                                                 lambda = 0.5 / (Ns * Ps),
                                                 use_cv = FALSE)
 
+fit_FCIR_ridgelambdacv <- estimate_penalized_FCIR(Y = Y[,,1],
+                                                  X = X,
+                                                  Tr = Tr,
+                                                  alpha = 0,
+                                                  lambda = "lambda.min",
+                                                  cv_group_by_site = TRUE,
+                                                  use_cv = TRUE)
+
+
 fit_FCIR_lassolambdacv <- estimate_penalized_FCIR(Y = Y[,,1],
                                                 X = X,
                                                 Tr = Tr,
@@ -74,21 +83,28 @@ data.frame(true = beta_0,
            unpen = fit_FCIR_unpen$beta_0, 
            ridge = fit_FCIR_ridgelambda$beta_0,
            lasso = fit_FCIR_lassolambda$beta_0,
+           ridgelambdacv = fit_FCIR_ridgelambdacv$beta_0,
            lassolambdacv = fit_FCIR_lassolambdacv$beta_0)
 data.frame(true = alpha_0, 
            unpen = fit_FCIR_unpen$alpha_0, 
            ridge = fit_FCIR_ridgelambda$alpha_0,
            lasso = fit_FCIR_lassolambda$alpha_0,
+           ridgelambdacv = fit_FCIR_ridgelambdacv$alpha_0,
            lassolambdacv = fit_FCIR_lassolambdacv$alpha_0)
 data.frame(true = B_mat %>% as.vector, 
            unpen = fit_FCIR_unpen$B_mat %>% as.vector,
            ridge = fit_FCIR_ridgelambda$B_mat %>% as.vector,
            lasso = fit_FCIR_lassolambda$B_mat %>% as.vector,
+           ridgelambdacv = fit_FCIR_ridgelambdacv$B_mat %>% as.vector,
            lassolambdacv = fit_FCIR_lassolambdacv$B_mat %>% as.vector)
 data.frame(true = A_mat %>% as.vector, 
            unpen = fit_FCIR_unpen$A_mat %>% as.vector,
            ridge = fit_FCIR_ridgelambda$A_mat %>% as.vector,
            lasso = fit_FCIR_lassolambda$A_mat %>% as.vector,
+           ridgelambdacv = fit_FCIR_ridgelambdacv$A_mat %>% as.vector,
            lassolambdacv = fit_FCIR_lassolambdacv$A_mat %>% as.vector)
 
 
+fit_FCIR_lassolambdacv$cv_model %>% plot
+#fit_FCIR_ridgelambdacv$cv_model %>% plot
+fit_FCIR_lassolambdacv$fold_id
