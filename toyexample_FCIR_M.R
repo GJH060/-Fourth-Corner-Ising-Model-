cef6b6eb63a5f803ad835100b9efbde64082f0b5
Francis KC Hui <fhui28@users.noreply.github.com>
2026-08-26 09:25:14 +1000

    farting around with weights in adaptive lasso

--- toyexample_FCIR_M.R
+++ toyexample_FCIR_M.R
@@ -67,11 +67,21 @@ fit_adaptivelasso <- estimate_adaptive_lasso_FCIR_M(Y = Y[,,1],
                                                     use_cv = TRUE,
                                                     cv_group_by_site = TRUE) 
 
+fit_adaptivelasso2 <- estimate_adaptive_lasso_FCIR_M(Y = Y[,,1],
+                                                    X = X,
+                                                    Tr,
+                                                    gamma = rep(c(1, 2), c(3, length(fit_unpen_nostan$glm_model$coefficients) - 3)), 
+                                                    lambda = "lambda.min",
+                                                    use_cv = TRUE,
+                                                    cv_group_by_site = TRUE) 
+
+
 d <- data.frame(true = t(beta_0 + tcrossprod(B_mat, Tr)) %>% as.vector,
                 ising_unpen = fit_ising_nopen$theta_jj,
                 ising_adlasso = fit_ising$theta_jj,
                 unpen = t(fit_unpen_nostan$beta_0 + tcrossprod(fit_unpen_nostan$B_mat, Tr)) %>% as.vector,
-                adlasso = t(fit_adaptivelasso$beta_0 + tcrossprod(fit_adaptivelasso$B_mat, Tr)) %>% as.vector)
+                adlasso = t(fit_adaptivelasso$beta_0 + tcrossprod(fit_adaptivelasso$B_mat, Tr)) %>% as.vector,
+                adlasso2 = t(fit_adaptivelasso2$beta_0 + tcrossprod(fit_adaptivelasso2$B_mat, Tr)) %>% as.vector)
 d
 d %>% t %>% dist
 
@@ -80,7 +90,8 @@ d <- data.frame(true = Theta_int[lower.tri(Theta_int)],
                 ising_unpen = fit_ising_nopen$Theta[lower.tri(fit_ising_nopen$Theta)],
                 ising_adlasso = fit_ising$Theta[lower.tri(fit_ising$Theta)],
                 unpen = fit_unpen_nostan$Theta_int[lower.tri(fit_unpen_nostan$Theta_int)], 
-                adlasso = fit_adaptivelasso$Theta_int[lower.tri(fit_adaptivelasso$Theta_int)]) 
+                adlasso = fit_adaptivelasso$Theta_int[lower.tri(fit_adaptivelasso$Theta_int)],
+                adlasso2 = fit_adaptivelasso2$Theta_int[lower.tri(fit_adaptivelasso2$Theta_int)]) 
 d 
 d %>% t %>% dist
 
@@ -95,4 +106,8 @@ d %>%
            adlasso = ifelse(adlasso != 0, 1, 0) %>% as.factor) %>% 
     xtabs(~ true + adlasso, data = .)
 
+d %>% 
+    mutate(true = ifelse(true != 0, 1, 0) %>% as.factor, 
+           adlasso2 = ifelse(adlasso2 != 0, 1, 0) %>% as.factor) %>% 
+    xtabs(~ true + adlasso2, data = .)
 
